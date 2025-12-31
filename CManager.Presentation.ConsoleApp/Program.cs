e81f3a8c837413f1d012d1d5dbcb6a242064c682
Emil L <emillarsson99@gmail.com>
2025-12-31 20:11:49 +0100

    Created businesslayer, infrastructurelayer and domainlayer, also emptied program.cs

--- CManager.Presentation.ConsoleApp/Program.cs
+++ CManager.Presentation.ConsoleApp/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace CManager.Presentation.ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        } 
-    }
-}
+﻿
