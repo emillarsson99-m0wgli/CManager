6496feeac974ed4de11685008d813908fe9a704e
Emil L <emillarsson99@gmail.com>
2026-01-02 17:48:24 +0100

    Created CustomerCreateRequest, CustomerResult and CustomerFactory.

--- CManager.Business/Services/CustomerService.cs
+++ CManager.Business/Services/CustomerService.cs
@@ -1,9 +1,12 @@
-﻿using CManager.Domain.Models;
+﻿using CManager.Domain.Factories;
+using CManager.Domain.Models;
 
 namespace CManager.Business.Services;
 
 public class CustomerService : ICustomerService
 {
+
+
     public bool CreateCustomer(string firstName, string lastName, string email, string phoneNumber, string StreetName, string postalCode, string city)
     {
         CustomerModel customerModel = new()
---
+++ CManager.Domain/Models/CustomerResult.cs
@@ -0,0 +1,7 @@
+﻿namespace CManager.Domain.Models;
+
+internal class CustomerResult
+{
+    public bool Success { get; set; }
+    public string? Message { get; set; }
+}
